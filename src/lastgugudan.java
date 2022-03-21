6322407ef273ef2d13a0ef8cc2fb07a1e11b1341
seok2zzangzzangman <seok2zzangzzangman@localhost>
2022-03-21 16:13:28 +0900

    if조건문연습

--- src/lastgugudan.java
+++ src/lastgugudan.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
 
 public class lastgugudan {
 public static void main(String[]args){
-for (int a = 1 ; a < 10 ; a++)
-System.out.println( a * 6 );
-}
-}
+System.out.println("출력할 구구단 단은? ; ");
+	//8,9
+	Scanner scanner = new Scanner(System.in);
+	int number = scanner.nextInt(); 
+	System.out.println("사용자가 입력한 값은 : "  +number);
+	
+			if (number<2) { 
+			System.out.println("2이상 9이하의 숫자를 입력하시오"); 
+			} else if (number>9) { 
+			System.out.println("2이상 9이하의 숫자를 입력하시오"); 
+			} else {
+				for (int i = 1 ; i < 10 ; i++) {
+					System.out.println(number*i); 
+				} 
+				}
+			}
+	}
+ 
+	
+	
+     
+
