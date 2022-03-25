e2a568d6d195d33df2dfe05231daa12f9a5d3f36
seok2zzangzzangman <seok2zzangzzangman@localhost>
2022-03-25 20:56:51 +0900

    배열 및 매소드 연습

--- src/lastgugudan.java
+++ src/lastgugudan.java
@@ -1,6 +1,33 @@
 
 public class lastgugudan {
-public static void main(String[]args){
+public static int[] abc(int times) {
+	int [] result = new int[9];
+	for(int i=0; i<result.length; i++) {
+		result[i]=times*(i+1);
+	}
+		return result;
+	}
+	public static void print(int[]result) {
+		for(int i=0; i<result.length ;i++){
+		System.out.println(result[i]);
+		}
+	}
+	public static void main(String[]args) {
+	for(int i=2;i<10;i++) {
+		int[] result=abc(i);
+	print (result);		
+		
+	}
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+
 	//ㅋㅋㅎㅎ
 //	int[] result = new int [10];
 //	for( int i=2 ; i < result.length ; i ++) 
@@ -8,12 +35,12 @@ public static void main(String[]args){
 //	System.out.println(a*i);	
 //	
 // 2단부터 9단 간소화시킴for문으로 	
-	for( int d = 2 ; d < 10 ;d++)
-	for( int a = 1 ; a < 10 ;a++)
-	System.out.println(a*d);	
-	
-	}
-}
+//	for( int d = 2 ; d < 10 ;d++)
+//	for( int a = 1 ; a < 10 ;a++)
+//	System.out.println(a*d);	
+//	
+//	}
+//}
 
 //연습
 
