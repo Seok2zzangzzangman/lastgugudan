15e7fedc9a63cbf5678e61c09ea8ca38b429588c
seok2zzangzzangman <seok2zzangzzangman@localhost>
2022-03-31 19:34:57 +0900

    배열은 해도해도 모르겠어ㅓ 먼 소리야 이게 도대체 ?

--- src/lastgugudan.java
+++ src/lastgugudan.java
@@ -1,80 +1,71 @@
-
-public class lastgugudan {
-public static int[] abc(int times) {
-	int [] result = new int[9];
-	for(int i=0; i<result.length; i++) {
-		result[i]=times*(i+1);
-	}
-		return result;
-	}
-	public static void print(int[]result) {
-		for(int i=0; i<result.length ;i++){
-		System.out.println(result[i]);
-		}
-	}
-	public static void main(String[]args) {
-	for(int i=2;i<10;i++) {
-		int[] result=abc(i);
-	print (result);		
-		
-	}
-	}
-}
-	
-	
-	
-	
-	
-	
-	
-
-	//ㅋㅋㅎㅎ
-//	int[] result = new int [10];
-//	for( int i=2 ; i < result.length ; i ++) 
-//	for( int a=1 ; a<10 ; a++)  
-//	System.out.println(a*i);	
-//	
-// 2단부터 9단 간소화시킴for문으로 	
-//	for( int d = 2 ; d < 10 ;d++)
-//	for( int a = 1 ; a < 10 ;a++)
-//	System.out.println(a*d);	
-//	
+//public class lastgugudan{
+//	public static int[] calculate(int times) { //calculate 라는 times라는 상수를 가진 배열 선언
+//		int[]result= new int[9]; // result 배열의 크기는 9 
+//		for(int i = 0 ; i < result.length ; i++) { //계산 결과를 배열에 저장 
+//			result[i] = times *(i+1); 
+//			}
+//		return result; // 반환 
+//	}
+//		
+//	public static void print(int[]result) { 
+//		for(int i = 0 ; i < result.length ; i++) { //계산 결과를 배열에 저장 
+//			System.out.println(result[i]);
+//			}	
+//	}		
+//	public static void main(String[]args) {
+//		
+//		for (int i = 1 ; i<10 ; i++) {
+//		
+//		int[]result= calculate(i);
+//		print(result);
+//		
+//		
 //	}
 //}
-
-//연습
-
-
-//		int[] result =new int[9];
-//		for( int i=0 ; i < result.length ; i++) {
-//		result[i] = 2*(i+1); 
-//			}
-//		for( int i = 0 ; i < result.length ; i++)
-//		for( int a = 1 ; a < 10 ; a++ )
-//		{
-//			System.out.println(result[i]);
+//}
+//public class lastgugudan{
+//	public static int[] calculate(int times) {
+//		int[]result = new int[9];
+//		for(int i=0 ; i<result.length ; i++) {
+//			result[i]=times*(1+i);
 //		}
-//		//3
-//			result =new int[9];
-//			for( int i=0 ; i < result.length ; i++) {
-//				result[i] = 3*(i+1); 
-//			}
-//			for( int i = 0 ; i < result.length ; i++) {
-//				System.out.println(result[i]);
-//		}
-//			//4단
-//			result =new int[9];
-//			for( int i=0 ; i < result.length ; i++) {
-//				result[i] = 4*(i+1); 
-//			}
-//			for( int i = 0 ; i < result.length ; i++) {
-//				System.out.println(result[i]);
-//			}
-//
-//			}
+//		return result;
 //	}
-// 
+//
+//public static void print(int[]result) {
+//	for(int i = 0 ; i < result.length ; i++) {
+//	System.out.println(result[i]);
+//	}
+//	}
+//public static void main(String[]args) {
+//	for ( int i = 1 ; i < 10 ; i++) {
+//		int[]result = calculate(i);
+//		print(result);
+//				
+//	
+//}
+//}
+//}
+public class lastgugudan{
+public static int[]calculate(int times){
+	int[]result=new int[9];
+	for (int i = 0 ; i < result.length ; i++ )
+		result[i]=times*(1+i);
+		return result;
 	
-	
-     
+}
 
+public static void print(int[]result) {
+	for ( int i = 0 ; i < result.length ; i++) {
+	System.out.println(result[i]);
+		
+	}
+}
+
+	public static void main(String[]args) {
+		for ( int i = 1 ; i < 10 ; i ++) {
+		int[] result = calculate(i);
+		print(result);
+	}
+	}
+	}
